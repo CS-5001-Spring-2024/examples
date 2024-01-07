e5841a31a70fa4ec617a99b1446eb6abf8f6bad3
Sami Rollins <srollins@gmail.com>
2024-01-07 18:57:23 -0500

    update problems for class on 1.8

---
+++ 1.8.2024/price_per_liter.py
@@ -0,0 +1,16 @@
+'''
+Suppose you are at the grocery store to buy soda and you want to get the best deal. Write a program that reads in the price of a six-pack of soda and the price of a two-liter bottle. The program should print out the price per liter for both assuming that cans are 12 oz or 0.355 liters. For example:
+
+Price per six-pack: 2.75
+Price per two-liter: 1.74
+
+Six-pack price per liter: 1.2910798122065728
+Two-liter price per liter: 0.87
+
+'''
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
---
+++ 1.8.2024/seconds_to_minutes.py
@@ -0,0 +1,23 @@
+"""
+A program to prompt a user to enter a number of seconds and 
+covert that to minutes.
+
+Examples:
+
+Enter a number of seconds: 60
+You entered 60 seconds.
+60 seconds is 1.0 minutes
+
+Enter a number of seconds: 90
+You entered 90 seconds.
+90 seconds is 1.5 minutes
+
+# https://docs.python.org/3/tutorial/inputoutput.html
+
+"""
+
+def main():
+	pass	
+
+if __name__ == '__main__':
+	main()	
