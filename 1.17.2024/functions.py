42c8fecc4b1108ab3c96aabcac984c88424ef040
Sami Rollins <srollins@gmail.com>
2024-01-15 11:21:35 -0500

    add functions exercises

---
+++ 1.17.2024/functions.py
@@ -0,0 +1,47 @@
+"""
+A python program to demonstrate how to use functions.
+"""
+
+def welcome():
+	"""Exercise 1: 
+	A function to print a multi-line welcome message.
+	You can print this documentation using 
+	print(welcome.__doc__)
+	"""
+	pass
+
+def greet_by_name(name: str):
+	"""Exercise 2:
+	A function to greet a user by name.
+	Parameters
+	name: str
+		name of the user
+	"""
+	pass
+
+def get_greeting() -> str:
+	"""Exercise 3:
+	A function to get a specific greeting from the user.
+	Returns:
+	str 
+		greeting provided by the user
+	"""
+	pass
+
+def welcome_by_name_with_greeting(name: str, greeting: str):
+	"""Exercise 4:
+	A function to welcome a user by name with a 
+	specific greeting.
+	Parameters:
+	name: str
+		name of the user
+	greeting: str
+		greeting to use
+	"""
+	pass
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
