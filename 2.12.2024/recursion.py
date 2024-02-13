eb352f53b6ae2c5cae65216f75486b5665341735
Sami Rollins <srollins@gmail.com>
2024-02-13 16:25:55 -0500

    update recursion examples

--- 2.12.2024/recursion.py
+++ 2.12.2024/recursion.py
@@ -7,7 +7,18 @@ def factorial_iterative(n: int) -> int:
 	A function that takes as input an integer and returns the 
 	factorial of that number.
 	"""
-	pass
+	# result = 1
+	# while n > 0:
+	# 	result = result * n
+	# 	n -= 1
+	# return result
+
+	result = 1
+	current = 1
+	while current <= n:
+		result *= current
+		current += 1
+	return result
 
 def factorial_recursive(n: int) -> int:
 	"""
@@ -15,7 +26,13 @@ def factorial_recursive(n: int) -> int:
 	factorial of that number.
 	This implementation does not use a loop.
 	"""
-	pass
+	# base case
+	if n == 1:
+		return 1
+
+	# recursive case
+	return n * factorial_recursive(n-1)
+
 
 def print_string(string: str):
 	"""
@@ -23,7 +40,17 @@ def print_string(string: str):
 	a str and prints the characters of the str one per 
 	line *without using a loop*. 
 	"""
-	pass
+	# base case -- empty string
+	# 		stop...return...nothing
+	if string == '':
+		return
+
+	# print a letter
+	print(string[0])
+
+	# recursively call
+	print_string(string[1:])
+
 
 def print_string_backward(string: str):
 	"""
@@ -31,7 +58,17 @@ def print_string_backward(string: str):
 	a str and prints the characters of the str *in 
 	reverse* one per line *without using a loop*. 
 	"""
-	pass	
+	# base case -- empty string
+	# 		stop...return...nothing
+	if string == '':
+		return
+
+	# recursively call
+	print_string_backward(string[1:])		
+
+	# print a letter
+	print(string[0])
+
 
 def print_nums_iterative(n: int):
 	"""
@@ -46,10 +83,23 @@ def print_nums_recursive(n: int):
 	There are several ways to do this. We will look at how to
 	use a helper function.
 	"""
-	pass
+	# if n == 0:
+	# 	return
+	
+	# print(n)
+	# print_nums_recursive(n-1)
+	
+	if n == 0:
+		return
+	print_nums_helper(1, n)
 
 def print_nums_helper(current: int, n: int):
-	pass
+	if current == n:
+		print(current)
+		return
+	print(current)
+	print_nums_helper(current + 1, n)
+
 
 def find_char_a(string: str) -> int:
 	"""
@@ -61,7 +111,6 @@ def find_char_a(string: str) -> int:
 	"""
 	pass
 
-map = {1: 'one', 2: 'two', 3: 'three', 4: 'four'}
 def in_english(number: int) -> str:
 	"""
 	Write a recursive function called in_english that takes a 
@@ -79,7 +128,11 @@ def binarysearch(list: list[int], target: int) -> bool:
  	pass
 
 def main():
-	pass
+	# print(factorial_recursive(3))
+	# print(factorial_recursive(5))
+	# print_string_backward('computer')
+	print_nums_recursive(5)
+
 
 if __name__ == '__main__':
 	main()
