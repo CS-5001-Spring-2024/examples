0d9920b3b91cd64ff67ad07b0ac4beca22f9f2ad
Sami Rollins <srollins@gmail.com>
2024-02-21 19:45:07 -0500

    update error handling examples

--- 2.21.2024/error_handling.py
+++ 2.21.2024/error_handling.py
@@ -15,7 +15,20 @@ def convert(value, target_type):
 	"""
 	# '5'
 	# int
-	pass
+
+	if target_type != int and target_type != float:
+		raise TypeError(f'Cannot convert to type {target_type}')
+
+	if isinstance(value, target_type):
+		return value
+
+	if target_type == int:
+		int_value = int(value)
+		return int_value
+
+	if target_type == float:
+		float_value = float(value)
+		return float_value
 
 
 def read_file(filename: str):
@@ -24,7 +37,9 @@ def read_file(filename: str):
 	The function will throw an exception if the file is
 	not found.
 	"""
-	pass
+	with open(filename, 'r') as input_file:
+		for line in input_file:
+			print(line)
 
 def search_replace(search:str, 
 	replace:str, 
@@ -45,7 +60,12 @@ def search_replace(search:str,
 	outfilename : str
 		the name of the file where the result will be saved
 	"""
-	pass
+	with open(infilename, 'r') as input_file, open(outfilename, 'w') as output_file:
+		for line in input_file:
+			result = line.replace(search, replace)
+			print(result)
+			output_file.write(result)
+
 
 def find_py_files(root: str):
 	# /Users/srollins/teaching/cs5001-f23 
@@ -53,11 +73,64 @@ def find_py_files(root: str):
 	Write a recursive function to list all files in any
 	descendant directory of a given root.
 	'''
-	pass
-
+	if os.path.isfile(root) and root.endswith('py'):
+		print(f'Python file: {root}')
+	elif os.path.isdir(root):
+		results = os.listdir(root)
+		for result in results:
+			find_py_files(os.path.join(root, result))
 
 def main():
-	pass
+
+
+	# try :
+	# 	search_replace('class', 'CS 5001', 'class_file.txt', 'out.txt')
+	# except FileNotFoundError as fnf:
+	# 	print(fnf)
+
+	# try :
+	# 	convert('123a', int)
+	# except TypeError as te:
+	# 	print(te)
+	# except ValueError as ve:
+	# 	print(ve)
+	# print('next line...')
+
+	# i = 0
+	# while i < 10:
+	# 	print(i)
+	# 	if i % 3 == 0:
+	# 		i += 1
+	# 		continue
+	# 	i += 2
+
+
+	# success = False
+	# while not success:
+	# 	number_str = input('enter a floating point number: ')
+	# 	try:
+	# 		number_float = float(number_str)
+	# 	except ValueError as ve:
+	# 		print(ve)
+	# 		continue
+	# 	except TypeError as te:
+	# 		print(te)
+
+	# 	success = True
+	# print(f'you said {number_float}')	
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
 	main()
