ecb1f1fddb71f8a6f641a3bdd11127bd23ae2f4b
Sami Rollins <srollins@gmail.com>
2024-04-10 18:56:58 -0400

    update hello example

--- 4.8.2024/message_board/hello.py
+++ 4.8.2024/message_board/hello.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 
 app = Flask(__name__)
+
+all_visitors = []
 
 @app.route('/hello')
 def hello():
 	name = request.args.get('name')
-
-	return f'<h1>Hello {name}!</h1><p>It is almost time to go home</p>'
+	all_visitors.append(name)
+	print(all_visitors)
+	return render_template('hello.html', name=name)
